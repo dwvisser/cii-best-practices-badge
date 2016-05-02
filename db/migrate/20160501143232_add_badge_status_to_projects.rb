8a612fd05cbdeed32d111ca00959f48d190645de
Dale Visser <dvisser@ida.org>
2016-05-02 11:12:56 -0400

    Added population of projects.badge_status to migration up.

--- db/migrate/20160501143232_add_badge_status_to_projects.rb
+++ db/migrate/20160501143232_add_badge_status_to_projects.rb
@@ -3,4 +3,15 @@ class AddBadgeStatusToProjects < ActiveRecord::Migration
     add_column :projects, :badge_status, :string
     add_index :projects, :badge_status
   end
+
+  def up
+    Project.find_each do |project|
+      project.update_badge_status
+      project.save
+    end
+  end
+
+  def down
+    # Having projects.badge_status column remain is OK.
+  end
 end
