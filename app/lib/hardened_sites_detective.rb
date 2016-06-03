c3e52ab3423ced6e74b11b7009a6cd5215f316ff
Dale Visser <dvisser@ida.org>
2016-06-03 15:56:11 -0400

    Added checking of X-Content-Type-Options value.

--- app/lib/hardened_sites_detective.rb
+++ app/lib/hardened_sites_detective.rb
@@ -3,10 +3,11 @@
 # frozen_string_literal: true
 
 class HardenedSitesDetective < Detective
+  XCTO = 'x-content-type-options'
+  NOSNIFF = 'nosniff'
   CHECK =
     [
-      'content-security-policy', 'x-content-type-options',
-      'x-frame-options', 'x-xss-protection'
+      'content-security-policy', XCTO, 'x-frame-options', 'x-xss-protection'
     ].freeze
   MET =
     {
@@ -14,22 +15,42 @@ class HardenedSitesDetective < Detective
       explanation: 'Found all required security hardening headers (values not'\
         'checked).'
     }.freeze
-  UNMET =
+  UNMET_MISSING =
     {
       value: 'Unmet', confidence: 5,
       explanation: 'At least one of the required security hardening headers is'\
         'missing.'
     }.freeze
+  UNMET_NOSNIFF =
+    {
+      value: 'Unmet', confidence: 5,
+      explanation: 'X-Content-Type-Options was not set to "nosniff".'
+    }.freeze
+
   INPUTS = %i(repo_url homepage_url).freeze
   OUTPUTS = [:hardened_site_status].freeze
 
-  def security_fields_present?(headers)
-    CHECK.reduce(true) { |a, e| a & headers.key?(e) }
+  def security_fields_present?(headers_list)
+    result = true
+    headers_list.each do |headers|
+      result &&= CHECK.reduce(true) { |a, e| a & headers.key?(e) }
+    end
+    result
   end
 
   def get_headers(evidence, url)
     response = evidence.get(url)
-    response.nil? ? {} : response[:metas]
+    headers = response.nil? ? {} : response[:meta]
+    headers.nil? ? {} : headers
+  end
+
+  def check_nosniff?(headers_list)
+    result = true
+    headers_list.each do |response_headers|
+      xcto = response_headers[XCTO]
+      result &&= xcto.nil? ? false : xcto.casecmp(NOSNIFF).zero?
+    end
+    result
   end
 
   def check_urls(evidence, homepage_url, repo_url)
@@ -37,9 +58,10 @@ class HardenedSitesDetective < Detective
     if !homepage_url.blank? && !repo_url.blank?
       homepage_headers = get_headers(evidence, homepage_url)
       repo_headers = get_headers(evidence, repo_url)
-      hardened = security_fields_present?(homepage_headers) &&
-                 security_fields_present?(repo_headers)
-      @results[:hardened_site_status] = hardened ? MET : UNMET
+      hardened = security_fields_present?([homepage_headers, repo_headers])
+      @results[:hardened_site_status] = hardened ? MET : UNMET_MISSING
+      hardened ||= check_nosniff?([homepage_headers, repo_headers])
+      @results[:hardened_site_status] = UNMET_NOSNIFF unless hardened
     end
     @results
   end
